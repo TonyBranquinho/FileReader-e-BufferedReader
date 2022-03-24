99a11c9f81757fb137cd124f2fd41dde90c91b32
Tony Branquinho <antony.branquinho@gmail.com>
2022-03-24 10:30:25 -0300

    soluçao boa

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,8 @@ public class Program {
 		// TODO Auto-generated method stub
 
 		File path = new File("c:\\nova\\in.txt");
-		FileReader fr = null;
-		BufferedReader br = null;
-
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 			String line = br.readLine();
 
 			while (line != null) {
@@ -26,18 +21,6 @@ public class Program {
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
-		} finally {
-			try {
-				if (br != null) {
-					br.close();
-				}
-				if (fr != null) {
-					fr.close();
-				}
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		} 
 	}
 }
